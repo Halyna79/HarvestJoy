96d78f1bce85f7c647391ad0320343950b41e051
halyna <galinabandurka7@gmail.com>
2025-02-16 11:28:22 +0100

    bag-modal

--- src/main.js
+++ src/main.js
@@ -1,1 +1,4 @@
-import './js/script.js';
+import { openModal, closeModal } from "./js/modal.js";
+
+const modalPrivacy = document.getElementById("modal-privacy");
+const modalCookie = document.getElementById("modal-cookie");
