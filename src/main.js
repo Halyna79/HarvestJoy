41486e3a529b6e11634b6459f9d74c70550de441
halyna <galinabandurka7@gmail.com>
2025-02-23 12:30:52 +0100

    smoothScroll

--- src/main.js
+++ src/main.js
@@ -3,3 +3,9 @@ import { setupFooterModals } from "./js/footer.js";
 document.addEventListener("DOMContentLoaded", function () {
     setupFooterModals();
 });
+
+import { enableSmoothScroll } from "./js/smoothScroll.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+    enableSmoothScroll();
+});
